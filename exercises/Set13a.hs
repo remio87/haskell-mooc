76909e271f2cdb6de02882d97f0989ed888131f5
remio87 <82135212+remio87@users.noreply.github.com>
2026-04-04 01:09:04 +0900

    complete set13a

--- exercises/Set13a.hs
+++ exercises/Set13a.hs
@@ -257,7 +257,7 @@ rob from to =
 --    ==> ((),7)
 
 update :: State Int ()
-update = todo
+update = state (\s -> ((), s * 2 + 1))
 
 ------------------------------------------------------------------------------
 -- Ex 8: Checking that parentheses are balanced with the State monad.
@@ -285,7 +285,19 @@ update = todo
 --   parensMatch "(()))("      ==> False
 
 paren :: Char -> State Int ()
-paren = todo
+paren c =
+  state
+    ( \s ->
+        if s == -1
+          then ((), -1)
+          else
+            if c == '('
+              then ((), s + 1)
+              else
+                if c == ')'
+                  then ((), s - 1)
+                  else ((), s)
+    )
 
 parensMatch :: String -> Bool
 parensMatch s = count == 0
@@ -317,7 +329,12 @@ parensMatch s = count == 0
 -- PS. The order of the list of pairs doesn't matter
 
 count :: (Eq a) => a -> State [(a, Int)] ()
-count x = todo
+count x = state $ \s ->
+  let lu = lookup x s
+      addOne (k, v) = if k == x then (k, v + 1) else (k, v)
+   in case lu of
+        Nothing -> ((), (x, 1) : s)
+        Just n -> ((), map addOne s)
 
 ------------------------------------------------------------------------------
 -- Ex 10: Implement the operation occurrences, which
@@ -339,4 +356,11 @@ count x = todo
 --    ==> (4,[(2,1),(3,1),(4,1),(7,1)])
 
 occurrences :: (Eq a) => [a] -> State [(a, Int)] Int
-occurrences xs = todo
+occurrences xs = do
+  mapM_ count xs
+  s <- get
+  return (length s)
+
+-- occurrences xs = state $ \s ->
+--   let (_, updatedState) = runState (mapM_ count xs) s
+--    in (length updatedState, updatedState)
